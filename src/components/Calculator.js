b334280fce66c400d4365da95721c4016a3d1a39
David Vergaray <david.vergaray.a@gmail.com>
2022-02-13 20:54:54 -0300

    Create Header component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from './Header';
 import calculate from './logic/calculate';
 
 const Style = {
@@ -22,42 +23,45 @@ function Calculator() {
   }
 
   return (
-    <div className="container">
-      <div className="row">
-        <div className={`result col-12 p-3 text-white text-end border-mid-gray ${darkGray}`}>
-          { state.next || state.operation || state.total || 0 }
+    <>
+      <Header />
+      <div className="container">
+        <div className="row">
+          <div className={`result col-12 p-3 text-white text-end border-mid-gray ${darkGray}`}>
+            { state.next || state.operation || state.total || 0 }
+          </div>
+        </div>
+        <div className="row">
+          <button type="button" onClick={() => press(obj, 'AC')} className={`col-3 ${button} ${lightGray}`}>AC</button>
+          <button type="button" onClick={() => press(obj, '+/-')} className={`col-3 ${button} ${lightGray}`}>+/-</button>
+          <button type="button" onClick={() => press(obj, '%')} className={`col-3 ${button} ${lightGray}`}>%</button>
+          <button type="button" onClick={() => press(obj, '÷')} className={`col-3 ${button} ${orange}`}>÷</button>
+        </div>
+        <div className="row">
+          <button type="button" onClick={() => press(obj, '7')} className={`col-3 ${button} ${lightGray}`}>7</button>
+          <button type="button" onClick={() => press(obj, '8')} className={`col-3 ${button} ${lightGray}`}>8</button>
+          <button type="button" onClick={() => press(obj, '9')} className={`col-3 ${button} ${lightGray}`}>9</button>
+          <button type="button" onClick={() => press(obj, 'x')} className={`col-3 ${button} ${orange}`}>x</button>
+        </div>
+        <div className="row">
+          <button type="button" onClick={() => press(obj, '4')} className={`col-3 ${button} ${lightGray}`}>4</button>
+          <button type="button" onClick={() => press(obj, '5')} className={`col-3 ${button} ${lightGray}`}>5</button>
+          <button type="button" onClick={() => press(obj, '6')} className={`col-3 ${button} ${lightGray}`}>6</button>
+          <button type="button" onClick={() => press(obj, '-')} className={`col-3 ${button} ${orange}`}>-</button>
+        </div>
+        <div className="row">
+          <button type="button" onClick={() => press(obj, '1')} className={`col-3 ${button} ${lightGray}`}>1</button>
+          <button type="button" onClick={() => press(obj, '2')} className={`col-3 ${button} ${lightGray}`}>2</button>
+          <button type="button" onClick={() => press(obj, '3')} className={`col-3 ${button} ${lightGray}`}>3</button>
+          <button type="button" onClick={() => press(obj, '+')} className={`col-3 ${button} ${orange}`}>+</button>
+        </div>
+        <div className="row">
+          <button type="button" onClick={() => press(obj, '0')} className={`col-6 ${button} ${lightGray}`}>0</button>
+          <button type="button" onClick={() => press(obj, '.')} className={`col-3 ${button} ${lightGray}`}>.</button>
+          <button type="button" onClick={() => press(obj, '=')} className={`col-3 ${button} ${orange}`}>=</button>
         </div>
       </div>
-      <div className="row">
-        <button type="button" onClick={() => press(obj, 'AC')} className={`col-3 ${button} ${lightGray}`}>AC</button>
-        <button type="button" onClick={() => press(obj, '+/-')} className={`col-3 ${button} ${lightGray}`}>+/-</button>
-        <button type="button" onClick={() => press(obj, '%')} className={`col-3 ${button} ${lightGray}`}>%</button>
-        <button type="button" onClick={() => press(obj, '÷')} className={`col-3 ${button} ${orange}`}>÷</button>
-      </div>
-      <div className="row">
-        <button type="button" onClick={() => press(obj, '7')} className={`col-3 ${button} ${lightGray}`}>7</button>
-        <button type="button" onClick={() => press(obj, '8')} className={`col-3 ${button} ${lightGray}`}>8</button>
-        <button type="button" onClick={() => press(obj, '9')} className={`col-3 ${button} ${lightGray}`}>9</button>
-        <button type="button" onClick={() => press(obj, 'x')} className={`col-3 ${button} ${orange}`}>x</button>
-      </div>
-      <div className="row">
-        <button type="button" onClick={() => press(obj, '4')} className={`col-3 ${button} ${lightGray}`}>4</button>
-        <button type="button" onClick={() => press(obj, '5')} className={`col-3 ${button} ${lightGray}`}>5</button>
-        <button type="button" onClick={() => press(obj, '6')} className={`col-3 ${button} ${lightGray}`}>6</button>
-        <button type="button" onClick={() => press(obj, '-')} className={`col-3 ${button} ${orange}`}>-</button>
-      </div>
-      <div className="row">
-        <button type="button" onClick={() => press(obj, '1')} className={`col-3 ${button} ${lightGray}`}>1</button>
-        <button type="button" onClick={() => press(obj, '2')} className={`col-3 ${button} ${lightGray}`}>2</button>
-        <button type="button" onClick={() => press(obj, '3')} className={`col-3 ${button} ${lightGray}`}>3</button>
-        <button type="button" onClick={() => press(obj, '+')} className={`col-3 ${button} ${orange}`}>+</button>
-      </div>
-      <div className="row">
-        <button type="button" onClick={() => press(obj, '0')} className={`col-6 ${button} ${lightGray}`}>0</button>
-        <button type="button" onClick={() => press(obj, '.')} className={`col-3 ${button} ${lightGray}`}>.</button>
-        <button type="button" onClick={() => press(obj, '=')} className={`col-3 ${button} ${orange}`}>=</button>
-      </div>
-    </div>
+    </>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,12 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header>
+    <header className="container-fluid px-sm-5 p-3 border border-2 border-dark">
       <nav className="d-flex justify-content-between">
         <h1>Math Magicians</h1>
-        <div className="d-flex justify-content-center">
-          <Link to="/calculator">Calculator</Link>
+        <div className="d-flex justify-content-center align-items-center">
+          <Link className="px-3" to="/">Home</Link>
+          <Link className="px-3" to="/calculator">Calculator</Link>
         </div>
       </nav>
     </header>
