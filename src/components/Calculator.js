7f14ae328e57b73b844636cda255227417a75acd
David Vergaray <david.vergaray.a@gmail.com>
2022-02-13 21:49:45 -0300

    Style Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -26,44 +26,46 @@ function Calculator() {
     <>
       <Header />
       <section id="calculator-section" className="container-fluid px-sm-5 p-3 screen-height">
-        <p>a</p>
-      </section>
-      <div id="calculator" className="container">
         <div className="row">
-          <div className={`result col-12 p-3 text-white text-end border-mid-gray ${darkGray}`}>
-            { state.next || state.operation || state.total || 0 }
+          <p className="p-3 col-12 col-md-5">Lets do some Math!</p>
+          <div id="calculator" className="container p-3">
+            <div className="row">
+              <div className={`result col-12 p-3 text-white text-end border-mid-gray ${darkGray}`}>
+                { state.next || state.operation || state.total || 0 }
+              </div>
+            </div>
+            <div className="row">
+              <button type="button" onClick={() => press(obj, 'AC')} className={`col-3 ${button} ${lightGray}`}>AC</button>
+              <button type="button" onClick={() => press(obj, '+/-')} className={`col-3 ${button} ${lightGray}`}>+/-</button>
+              <button type="button" onClick={() => press(obj, '%')} className={`col-3 ${button} ${lightGray}`}>%</button>
+              <button type="button" onClick={() => press(obj, '÷')} className={`col-3 ${button} ${orange}`}>÷</button>
+            </div>
+            <div className="row">
+              <button type="button" onClick={() => press(obj, '7')} className={`col-3 ${button} ${lightGray}`}>7</button>
+              <button type="button" onClick={() => press(obj, '8')} className={`col-3 ${button} ${lightGray}`}>8</button>
+              <button type="button" onClick={() => press(obj, '9')} className={`col-3 ${button} ${lightGray}`}>9</button>
+              <button type="button" onClick={() => press(obj, 'x')} className={`col-3 ${button} ${orange}`}>x</button>
+            </div>
+            <div className="row">
+              <button type="button" onClick={() => press(obj, '4')} className={`col-3 ${button} ${lightGray}`}>4</button>
+              <button type="button" onClick={() => press(obj, '5')} className={`col-3 ${button} ${lightGray}`}>5</button>
+              <button type="button" onClick={() => press(obj, '6')} className={`col-3 ${button} ${lightGray}`}>6</button>
+              <button type="button" onClick={() => press(obj, '-')} className={`col-3 ${button} ${orange}`}>-</button>
+            </div>
+            <div className="row">
+              <button type="button" onClick={() => press(obj, '1')} className={`col-3 ${button} ${lightGray}`}>1</button>
+              <button type="button" onClick={() => press(obj, '2')} className={`col-3 ${button} ${lightGray}`}>2</button>
+              <button type="button" onClick={() => press(obj, '3')} className={`col-3 ${button} ${lightGray}`}>3</button>
+              <button type="button" onClick={() => press(obj, '+')} className={`col-3 ${button} ${orange}`}>+</button>
+            </div>
+            <div className="row">
+              <button type="button" onClick={() => press(obj, '0')} className={`col-6 ${button} ${lightGray}`}>0</button>
+              <button type="button" onClick={() => press(obj, '.')} className={`col-3 ${button} ${lightGray}`}>.</button>
+              <button type="button" onClick={() => press(obj, '=')} className={`col-3 ${button} ${orange}`}>=</button>
+            </div>
           </div>
         </div>
-        <div className="row">
-          <button type="button" onClick={() => press(obj, 'AC')} className={`col-3 ${button} ${lightGray}`}>AC</button>
-          <button type="button" onClick={() => press(obj, '+/-')} className={`col-3 ${button} ${lightGray}`}>+/-</button>
-          <button type="button" onClick={() => press(obj, '%')} className={`col-3 ${button} ${lightGray}`}>%</button>
-          <button type="button" onClick={() => press(obj, '÷')} className={`col-3 ${button} ${orange}`}>÷</button>
-        </div>
-        <div className="row">
-          <button type="button" onClick={() => press(obj, '7')} className={`col-3 ${button} ${lightGray}`}>7</button>
-          <button type="button" onClick={() => press(obj, '8')} className={`col-3 ${button} ${lightGray}`}>8</button>
-          <button type="button" onClick={() => press(obj, '9')} className={`col-3 ${button} ${lightGray}`}>9</button>
-          <button type="button" onClick={() => press(obj, 'x')} className={`col-3 ${button} ${orange}`}>x</button>
-        </div>
-        <div className="row">
-          <button type="button" onClick={() => press(obj, '4')} className={`col-3 ${button} ${lightGray}`}>4</button>
-          <button type="button" onClick={() => press(obj, '5')} className={`col-3 ${button} ${lightGray}`}>5</button>
-          <button type="button" onClick={() => press(obj, '6')} className={`col-3 ${button} ${lightGray}`}>6</button>
-          <button type="button" onClick={() => press(obj, '-')} className={`col-3 ${button} ${orange}`}>-</button>
-        </div>
-        <div className="row">
-          <button type="button" onClick={() => press(obj, '1')} className={`col-3 ${button} ${lightGray}`}>1</button>
-          <button type="button" onClick={() => press(obj, '2')} className={`col-3 ${button} ${lightGray}`}>2</button>
-          <button type="button" onClick={() => press(obj, '3')} className={`col-3 ${button} ${lightGray}`}>3</button>
-          <button type="button" onClick={() => press(obj, '+')} className={`col-3 ${button} ${orange}`}>+</button>
-        </div>
-        <div className="row">
-          <button type="button" onClick={() => press(obj, '0')} className={`col-6 ${button} ${lightGray}`}>0</button>
-          <button type="button" onClick={() => press(obj, '.')} className={`col-3 ${button} ${lightGray}`}>.</button>
-          <button type="button" onClick={() => press(obj, '=')} className={`col-3 ${button} ${orange}`}>=</button>
-        </div>
-      </div>
+      </section>
     </>
   );
 }
